907c06dc2fe7bec051260564ac2cad338eb5349f
Natalie Kaufhold <natalie.kaufhold@arcor.de>
2017-06-15 11:52:40 +0200

    Translate the comments into English
    Translated the German comments into English

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,36 +1,36 @@
-﻿/* Übung Ein- und Ausgabe & Dynamischer Speicher */
-#include <iostream>  // Bibliothek (standard)
-#include <sstream>   // String stream Bibliothek
-using namespace std; // Müsste sonst immer std:: verwenden
+﻿/* Task: Input Output & dynamic memory */
+#include <iostream>
+#include <sstream>
+using namespace std;
 
-/*****- Main Funktion -****/
-int main(int argc, char *argv[]) // Die Anzahl muss erst definiert werden // main() nimmt nur 0 oder 2 Argumente! // Syntax ist fest vorgeschrieben!!
+/*****- Main function -****/
+int main(int argc, char *argv[]) // the number has to be defined // main() only takes 0 or 2 arguments
 {
 
-	/******- Variablen -********/
+	/******- variables -********/
 	double temp = 0;
 
-	int N = argc - 1;		// argc = Anzahl der Argumente, die der Main() übergeben werden
-	double *data;			// Dynamischer Vektor zum Speichern der Werte // BEACHTE: immer Pointer!
-	data = new double[N];	// Zuteilen des Speichers
+	int N = argc - 1;		// argc = number of the arguments, which were given to the main
+	double *data;			// dynamically vector to storage the input // ALWAYS pointer
+	data = new double[N];		// create an array and tell it the storage address
 
 	/******- Code -************/
 	for (int l = 1; l < N + 1; l++)
 	{
 
-		stringstream s;		// Stringstream zum "Konvertieren" des Pointers in string (Als Puffer!)
-		s << argv[l];		// Speichern in den Puffer
+		stringstream s;		// Stringstream to convert the double to a string in order to print it
+		s << argv[l];		
 
-		s >> data[l - 1];	// -1, weil sonst außerhalb des Arrays
+		s >> data[l - 1];	
 	}
 	cout << endl;
 
-	/* Sortieren (BubbleSort) Aufgabe 4 */
+	/*  BubbleSort */
 	for (int k = 0; k < N; k++)
 	{
-		for (int i = 0; i < N - 1; i++) // Er vergleicht immer einmal weniger!!!
+		for (int i = 0; i < N - 1; i++) 
 		{
-			if (data[i] > data[i + 1]) // Nicht inkrementieren!!
+			if (data[i] > data[i + 1])
 			{
 				temp = data[i + 1];
 				data[i + 1] = data[i];
@@ -39,11 +39,11 @@ int main(int argc, char *argv[]) // Die Anzahl muss erst definiert werden // mai
 		}
 	}
 
-	/*****- Ausgabe -****/
+	/*****- Print the output -****/
 	for (int j = 0; j < N; j++)
 	{
 		cout << data[j] << endl;
 	}
 
-	return 0; // Immer einen Wert zurückgeben!
+	return 0; // the main needs a return, because it's not void
 }
